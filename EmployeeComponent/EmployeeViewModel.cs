1dfa4c14fc2c1404e64a6804a22fa905ca10d201
Yudeyue <87197565+Yudeyue@users.noreply.github.com>
2023-10-24 14:41:13 +0300

    Add files via upload

---
+++ EmployeeComponent/EmployeeViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeComponent
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+
+        private string _lastName= string.Empty;
+        private string _firstname = string.Empty;
+        public string LastName 
+        {
+            get { return _lastName; }
+            set
+            {
+                if (value == _lastName) return;
+                _lastName = value;
+                NotifyPropertyChanged("LastName");
+            }
+        }
+
+        public char Gender { get; set; }    
+
+        public bool IsManager { get; set; }
+
+        public decimal AnnualSalary { get; set; }
+
+        public string FirstName
+        {
+            get { return _firstname; }
+            set
+            {
+                if (_firstname == value) return;
+                _firstname = value;
+                NotifyPropertyChanged("FirstName");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ EmployeeComponent/Employees.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeComponent
+{
+    public class Employees
+    {
+        public ObservableCollection<EmployeeViewModel> GetEmployees() 
+        { 
+            var employees = new ObservableCollection<EmployeeViewModel>();
+            employees.Add(new EmployeeViewModel
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Yu",
+                AnnualSalary = 40000,
+                Gender = 'm',
+                IsManager = false
+            });
+            employees.Add(new EmployeeViewModel
+            {
+                Id = 2,
+                FirstName = "Sally",
+                LastName = "Yu",
+                AnnualSalary = 55000,
+                Gender = 'f',
+                IsManager = true
+            });
+            employees.Add(new EmployeeViewModel
+            {
+                Id = 3,
+                FirstName = "Bill",
+                LastName = "Yu",
+                AnnualSalary = 30000,
+                Gender = 'm',
+                IsManager = false
+            });
+            employees.Add(new EmployeeViewModel
+            {
+                Id = 4,
+                FirstName = "Jamie",
+                LastName = "Yu",
+                AnnualSalary = 80000,
+                Gender = 'm',
+                IsManager = true
+            });
+            return employees;
+        }
+    }
+}
